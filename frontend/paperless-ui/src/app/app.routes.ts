350c43566de643c24ff533b95713dd42eb5ef8da
Beri <if23b123@technikum-wien.at>
2025-09-29 23:31:10 +0200

    Basic structure of new frontend established

--- frontend/paperless-ui/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent} from "./pages/dashboard/dashboard.component";
-import { DetailComponent} from "./pages/detail/detail.component";
-
-export const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'document/:id', component: DetailComponent },
-];
