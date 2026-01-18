6d4ed10635eec3ac353294f7c38dcbb43aa6314e
Beri <if23b123@technikum-wien.at>
2026-01-18 17:56:39 +0100

    Removal of unused files in documentaccessbatchservice

--- document-access-batch-service/src/main/java/fhtw/wien/documentaccessbatchservice/config/ChunksConfig.java
+++ document-access-batch-service/src/main/java/fhtw/wien/documentaccessbatchservice/config/ChunksConfig.java
@@ -1,26 +1,18 @@
 package fhtw.wien.documentaccessbatchservice.config;
 
 import fhtw.wien.documentaccessbatchservice.batch.processor.DocumentAccessItemProcessor;
-import fhtw.wien.documentaccessbatchservice.batch.reader.DocumentAccessItemReader;
 import fhtw.wien.documentaccessbatchservice.batch.writer.DocumentAccessItemWriter;
-import fhtw.wien.documentaccessbatchservice.messaging.DocumentAccessStatProducer;
 import fhtw.wien.documentaccessbatchservice.dto.DocumentAccessStatDto;
-import fhtw.wien.documentaccessbatchservice.xmlModel.AccessStatisticsDateListener;
 import fhtw.wien.documentaccessbatchservice.xmlModel.DocumentAccessXml;
 import jakarta.xml.bind.JAXBException;
 import org.springframework.batch.core.*;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.file.MultiResourceItemReader;
-import org.springframework.batch.item.xml.StaxEventItemReader;
-import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
